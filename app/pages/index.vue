a8af9d57d49fa83b42aa753f4f87cf54c6ad2dbc
Nacha <ignaciaclaren@gmail.com>
2026-06-27 19:49:39 -0400

    modal para inscribirse

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,14 +2,43 @@
 import type { Evento } from '~/types/evento'
     const { data: eventos, pending, error, refresh } = await useFetch<Evento[]>('/api/eventos')
     const eventoSeleccionado = ref<Evento | null>(null);
+    const datos = ref({
+        nombre: '',
+        apellido: '',
+        email: ''
+    })
+
     function abrirModalInscripcion(evento: Evento) {
         eventoSeleccionado.value = evento
+    }
+
+    function cerrarModalInscripccion(){
+        eventoSeleccionado.value = null
+    }
+
+    async function inscribir(){
+        try {
+            await $fetch('/api/inscrito', {
+                method: 'POST',
+                body: {
+                    email: datos.value.email,
+                    nombre: datos.value.nombre,
+                    apellido: datos.value.apellido,
+                    eventoId: eventoSeleccionado.value?.id,
+                }
+            })
+            alert("inscrito con exito!")
+            cerrarModalInscripccion()
+            await refresh()
+        }catch (err: any) {
+            console.error(err)
+        }
     }
 </script>
 <template>
     <div class="space-y-8">
         <h1> eventos </h1>
-
+        {{ datos }}
         <div v-for="evento in eventos">
             <h2>
               {{evento.titulo}}
@@ -18,18 +47,20 @@ import type { Evento } from '~/types/evento'
             <p>lugar: {{evento.lugar}} </p>
             <p>inscritos: {{evento.inscritos.length}} </p>
             <b>valor:  {{evento.valor}} </b>
-            <button @click="abrirModalInscripcion">
+            <button @click="abrirModalInscripcion(evento)">
                 Inscrbirse
             </button>
         </div>
 
         <div v-if="eventoSeleccionado" class="modal-overlay">
             <div class="modal-content">
-                <h3>Inscribirse a: {{ eventoSeleccionado?.titulo }}</h3>
-                <p>¿Estás seguro que deseas inscribirte?</p>
-                
+                <h3>ingresa tus datos : {{ eventoSeleccionado?.titulo }}</h3>
+                <input type='text' v-model="datos.nombre" placeholder="nombre" > 
+                <input type='text' v-model="datos.apellido" placeholder="apellido" > 
+                <input type='text' v-model="datos.email" placeholder="email" > 
+                <button @click="inscribir">Confirmar Inscripción</button>
                 <button @click="eventoSeleccionado = null">Cancelar</button>
-                <button>Confirmar Inscripción</button>
+
             </div>
         </div>
     </div>
