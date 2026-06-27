e99b7cb29b79e0ac5217ace68d17a9c4fd8e090d
Ignacia Vargas <ignaciavargas@MacBook-Pro-de-Ignacia.local>
2026-06-27 18:34:24 -0400

    trae los eventos de la BD

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,19 @@
-<template>
-    <!--file:///Users/ignaciavargas/Downloads/recursos%202/vistas%20html/src/eventos.html-->
-    <div class="space-y-8">
-        <h1>
-            eventos
-        </h1>
+<script setup lang="ts">
+import type { Evento } from '~/types/evento'
+    const { data: eventos, pending, error, refresh } = await useFetch<Evento[]>('/api/eventos')
 
+</script>
+<template>
+    <div class="space-y-8">
+        <h1> eventos </h1>
+
+        <div v-for="evento in eventos">
+            <h2>
+              {{evento.titulo}}
+            </h2>
+            <p>fecha: {{evento.fecha}} </p>
+            <p>lugar: {{evento.lugar}} </p>
+            <b>valor:  {{evento.valor}} </b>
+        </div>
     </div>
 </template>
---
+++ app/types/evento.ts
@@ -0,0 +1,7 @@
+export interface Evento{
+    id: number
+    titulo: string
+    fecha: Date
+    lugar: string
+    valor: number
+}
