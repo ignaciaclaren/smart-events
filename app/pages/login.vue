f1ae62c05a0a5d7b475ec923c163deac9ec2a1f8
Nacha <ignaciaclaren@gmail.com>
2026-06-30 05:49:37 -0400

    barra de navegacion

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import { ref } from 'vue';
     const datos = ref({
         email: '',
         password: ''
     })
+
     definePageMeta({
         middleware: ['staff']
     })
@@ -27,6 +29,19 @@
 </script>
 
 <template>
+    <header class="border-b border-neon-blue bg-neon-bg">
+            <div class="mx-auto flex max-w-6xl items-center justify-between px-6 py-4">
+                <NuxtLink to = "/" class="text-xl font-black text-neon-blue ">
+                    Smart Events
+                </NuxtLink>
+
+                <nav>
+                    <NuxtLink to="/login" class="rounded-lg px-4 py-2 text-sm font-bold text-neon-green border border-neon-green hover:bg-neon-green hover:text-neon-bg transition-all">
+                        Staff
+                     </NuxtLink>
+                </nav>
+            </div>
+        </header>
     <div class="min-h-screen bg-[color:var(--neon-bg)] text-white flex flex-col items-center justify-center p-6">
         
         <div class="w-full max-w-sm bg-[#111113] border border-gray-800 rounded-2xl p-8 shadow-2xl">
@@ -36,7 +51,7 @@
                 <p class="text-gray-400 text-sm mt-2">Inicia sesión para continuar</p>
             </div>
             
-            <div >
+            <div class="flex flex-col space-y-4">
                 <input 
                     type="email" 
                     placeholder="Email" 
@@ -48,9 +63,9 @@
                     v-model="datos.password"
                     class="w-full p-4 bg-black border border-gray-700 rounded-xl focus:border-[color:var(--neon-blue)] focus:outline-none transition-all">
                 
-                <button @click="login"
+                <button @click="login" 
                     type="submit"
-                    class="w-full py-4 mt-4 bg-[color:var(--neon-blue)] text-white font-bold rounded-xl hover:opacity-90 transition-all">
+                    class="bg-neon-bg border border-neon-blue text-neon-blue px-6 py-2 rounded-lg transition-all duration-300 hover:bg-neon-blue hover:text-black hover:border-neon-blue">
                     Entrar
                 </button>
             </div>
