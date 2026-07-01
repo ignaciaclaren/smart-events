cea6e4d6d806cb215da407a802881f22182983f5
Nacha <ignaciaclaren@gmail.com>
2026-06-30 20:55:01 -0400

    formato datios usuario

--- server/api/usuarios/index.post.ts
+++ server/api/usuarios/index.post.ts
@@ -3,6 +3,9 @@ export default defineEventHandler(async (event) => {
     const body = await readBody(event)
     const hash = await bcrypt.hash(body.password, 12);
     body.password = hash;
+    body.nombre = body.nombre.trim().toUpperCase();
+    body.apellido = body.apellido.trim().toUpperCase();
+    body.email = body.email.trim().toUpperCase();
     return await prisma.usuario.create({
         data: body
     })
